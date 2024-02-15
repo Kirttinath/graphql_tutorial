74ce6f33787fc6ebf2d084fb5039558d38490bf5
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-15 12:54:49 +0530

    single endpoint checking in apolloServer

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+//? import db
+import db from "./_db.js";
 
 //? import typeDefs
 import { typeDefs } from "./schema.js";
 
 //Todo Server SetUp :
-
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    authors() {
+      return db.authors;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    review(_, args) {
+      //? 3 parameters parent, args and context
+      return db.reviews.find((review) => review.id === args.id);
+    },
+    author(_, args) {
+      //? 3 parameters parent, args and context
+      return db.authors.find((author) => author.id === args.id);
+    },
+    game(_, args) {
+      //? 3 parameters parent, args and context
+      return db.games.find((game) => game.id === args.id);
+    },
+  },
+};
 const server = new ApolloServer({
   //* typeDefs : Definitions of types of data
   typeDefs,
   //* Resolvers
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
--- schema.js
+++ schema.js
@@ -10,7 +10,7 @@ type Review {
     content: String!
 }
 type Author {
-    id: Id!,
+    id: ID!,
     name: String!,
     verified: Boolean!
 }
@@ -18,5 +18,8 @@ type Query {
     reviews:[Review]
     games: [Game]
     authors: [Author]
+    review(id:ID!) : Review
+    game(id:ID!) : Game
+    author(id:ID!) : Author
 }
 `;
