6569044c88349526fe934d2d848ec15160427ef3
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-15 11:38:51 +0530

    server setup

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+
+//Todo Server SetUp :
+
+const server = new ApolloServer({
+  //* typeDefs : Definitions of types of data
+  //* Resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log("server is ready", 4000);
